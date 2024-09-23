36a2889f65c202eee9a12004b28a3f1bc03d47eb
snowykte0426 <snowykte0426@naver.com>
2024-09-23 23:16:53 +0900

    feat: AuthDetailsServiceImpl 구현
    클래스만 선언되어 있던 AuthDetailsServiceImpl를 실질적으로 구현하였습니다
    
    Fixes: #2

--- src/main/kotlin/com/jova/global/auth/service/Impl/AuthDetailsServiceImpl.java
+++ src/main/kotlin/com/jova/global/auth/service/Impl/AuthDetailsServiceImpl.java
@@ -1,4 +1,42 @@
 package com.jova.global.auth.service.Impl;
 
-public class AuthDetailsServiceImpl {
+import com.jova.domain.auth.entity.Auth;
+import com.jova.domain.auth.repository.AuthRepository;
+import com.jova.global.auth.service.AuthDetailsService;
+import com.jova.global.auth.userdetails.AuthDetails;
+import com.jova.global.exception.ErrorCode;
+import com.jova.global.exception.JovaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthDetailsServiceImpl implements AuthDetailsService {
+    private static final Logger logger = LoggerFactory.getLogger(AuthDetailsService.class);
+    private final AuthRepository authRepository;
+
+    private AuthDetailsServiceImpl(AuthRepository authRepository) {
+        this.authRepository = authRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String id) {
+        logger.info("Loading User by ID: {}", id);
+        UUID uuid;
+        try {
+            uuid = UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            logger.error("Invalid UUID format for ID: {}", id);
+            throw new JovaException(ErrorCode.USER_NOT_FOUND);
+        }
+        Auth auth = authRepository.findById(uuid).orElseThrow(() -> {
+            logger.error("User not found for ID: {}", id);
+            return new JovaException(ErrorCode.USER_NOT_FOUND);
+        });
+        logger.info("User found for ID: {}", id);
+        return new AuthDetails(auth);
+    }
 }
