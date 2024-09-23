b22ffd86c4038c7dc18ef8b167d5e0cc8de9d8f3
snowykte0426 <snowykte0426@naver.com>
2024-09-23 23:18:01 +0900

    refactor: AuthRepository 리팩토링
    내부 메서드명을 변경하고 명시적으로 ``@Repository`` 어노테이션을 사용했습니다
    
    Fixes: #2

--- src/main/kotlin/com/jova/domain/auth/repository/AuthRepository.java
+++ src/main/kotlin/com/jova/domain/auth/repository/AuthRepository.java
@@ -2,15 +2,19 @@ package com.jova.domain.auth.repository;
 
 import com.jova.domain.auth.entity.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthRepository extends JpaRepository<Auth, Long> {
-    Auth findByname(String username);
+import java.util.UUID;
+
+@Repository
+public interface AuthRepository extends JpaRepository<Auth, UUID> {
+    Auth findByName(String name);
 
     Auth findByEmail(String email);
 
-    Auth findBynameOrEmail(String username, String email);
+    Auth findByNameOrEmail(String name, String email);
 
-    Boolean existsByname(String username);
+    Boolean existsByName(String name);
 
     Boolean existsByEmail(String email);
 }
