466c75e977c9fa790fb5fb1d554d0d63742558fd
snowykte0426 <snowykte0426@naver.com>
2024-09-25 14:15:09 +0900

    feat: 계정 정보 조회 API 추가
    JWT 토큰을 이용해 계정정보를 추출하여 반환하는 엔드포인트를 추가했습니다
    
    Resolves: #3

--- src/main/kotlin/com/jova/domain/auth/controller/AuthController.java
+++ src/main/kotlin/com/jova/domain/auth/controller/AuthController.java
@@ -2,9 +2,12 @@ package com.jova.domain.auth.controller;
 
 import com.jova.domain.auth.dto.request.SignInRequest;
 import com.jova.domain.auth.dto.response.TokenResponse;
+import com.jova.domain.auth.entity.Auth;
+import com.jova.domain.auth.service.AuthInfoService;
 import com.jova.domain.auth.service.LogoutService;
 import com.jova.domain.auth.service.ReissueTokenService;
 import com.jova.domain.auth.service.SignInService;
+import com.jova.global.security.jwt.service.JwtProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -28,6 +31,10 @@ public class AuthController {
     private final ReissueTokenService reissueTokenService;
     @Autowired
     private final LogoutService logoutService;
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private AuthInfoService authInfoService;
 
     @Operation(summary = "로그인", description = "GAuth를 이용한 로그인을 수행하는 API")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "로그인 성공"), @ApiResponse(responseCode = "400", description = "로그인 실패")})
@@ -51,5 +58,12 @@ public class AuthController {
     public ResponseEntity<Void> logout(@RequestHeader("Authorization") String accessToken) {
         logoutService.logout(accessToken);
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "계정 정보확인", description = "계정 정보를 확인하는 API")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "계정 정보 확인 성공"), @ApiResponse(responseCode = "401", description = "인증 실패")})
+    @GetMapping
+    public Auth getAuthInfo(@RequestHeader("Authorization") String accessToken) {
+        return authInfoService.getAuthInfo(accessToken);
     }
 }
