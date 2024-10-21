beaf1c3416a18130ff2ad9d5561c389241228193
jihoonwjj <wangjajihooon@gmail.com>
2024-10-21 17:20:42 +0900

    feat: 빌더 패턴 추가 Related to: #25

--- src/main/kotlin/com/jova/domain/article/entity/ArticleEntity.java
+++ src/main/kotlin/com/jova/domain/article/entity/ArticleEntity.java
@@ -1,29 +1,38 @@
 package com.jova.domain.article.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 import java.time.LocalDateTime;
 
+
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+
 public class ArticleEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, name="article_title")
     private String title;
+    @Column(nullable = false, name="article_content")
     private String content;
+    @Column(nullable = false, name="article_category")
     private String category;
+    @Column(nullable = false, name="article_time")
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    /*@Builder
+    @Builder
     public ArticleEntity(String title, String content, String category, LocalDateTime createdAt) {
         this.title = title;
         this.content = content;
         this.category = category;
         this.createdAt = createdAt != null ? createdAt : LocalDateTime.now();
-    }*/
+    }
 }
 
