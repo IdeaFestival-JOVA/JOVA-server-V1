93362e57cfa4aa82a775985d7e72714bb960fe0d
jihoonwjj <wangjajihooon@gmail.com>
2024-10-25 21:31:57 +0900

    feat: 지원서 컨트롤러 추가

---
+++ src/main/kotlin/com/jova/domain/application/controller/ApplicationController.java
@@ -0,0 +1,9 @@
+package com.jova.domain.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("articles/{id}/application")
+public class ApplicationController {
+}
