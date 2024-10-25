36d6a1fbc5f24db69795deade392e3e19a5ac15c
jihoonwjj <wangjajihooon@gmail.com>
2024-10-26 01:37:41 +0900

    feat: Auth로 Application 찾는 기능추가

--- src/main/kotlin/com/jova/domain/application/repository/ApplicationRepository.java
+++ src/main/kotlin/com/jova/domain/application/repository/ApplicationRepository.java
@@ -1,12 +1,16 @@
 package com.jova.domain.application.repository;
 
 import com.jova.domain.application.entity.Application;
+import com.jova.domain.article.entity.Article;
+import com.jova.domain.auth.entity.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    Optional<Application> findByArticleApplicationId(Long article_id, Long application_id);
+    Optional<Application> findByArticle(Article article);
+    List<Application> findByAuth(Auth auth);
 }
