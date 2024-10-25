f3414997836801e1fba7b44bca7cf09cb968e82b
jihoonwjj <wangjajihooon@gmail.com>
2024-10-25 21:42:57 +0900

    feat: 외래키 설정

--- src/main/kotlin/com/jova/domain/article/entity/ArticleEntity.java
+++ src/main/kotlin/com/jova/domain/article/entity/ArticleEntity.java
@@ -1,14 +1,14 @@
 package com.jova.domain.article.entity;
 
+import com.jova.Application;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -26,6 +26,9 @@ public class ArticleEntity {
     private String category;
     @Column(nullable = false, name="article_time")
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Application> applications = new ArrayList<>();
 
     @Builder
     public ArticleEntity(String title, String content, String category, LocalDateTime createdAt) {
