6ffb362c04dc9c69b97934207ac50106de8dc58d
jihoonwjj <wangjajihooon@gmail.com>
2024-12-16 09:09:40 +0900

    refactor: Builder 삭제

--- src/main/kotlin/com/jova/domain/application/entity/Application.java
+++ src/main/kotlin/com/jova/domain/application/entity/Application.java
@@ -1,25 +1,25 @@
 package com.jova.domain.application.entity;
 
-import com.jova.domain.article.entity.Article;
 import com.jova.domain.auth.entity.Auth;
 import com.jova.domain.user.UserMajor;
 import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity(name="application")
+@Entity(name="application_entity")
 @NoArgsConstructor
 @Getter
+@Builder
+@AllArgsConstructor
 public class Application {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    private Article article;
 
     @Column(name="유저전공",nullable = false, unique = true)
     private UserMajor major;
@@ -31,21 +31,7 @@ public class Application {
     @JoinColumn(name="authid", nullable=false)
     private Auth auth;
 
-//    @OneToMany(fetch = FetchType.LAZY)
-//    @JoinColumn(name="user_major")
-//    private UserMajor userMajor;
-
     @Column(name="applied_time", nullable=false)
-    private LocalDateTime appliedAt;
-
-    @Builder
-    public Application(Article article, LocalDateTime appliedAt, Auth auth, String content, UserMajor major) {
-        this.article = article;
-        this.appliedAt = appliedAt != null ? appliedAt : LocalDateTime.now();
-        this.auth = auth;
-        this.content = content;
-        this.major = major;
-    }
-
+    private LocalDateTime createdAt;
 
 }
