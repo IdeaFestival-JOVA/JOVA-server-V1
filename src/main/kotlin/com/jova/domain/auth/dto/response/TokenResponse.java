e29aaa8bdbcab9e2470bc220cc6481f71c9e2ef9
jihoonwjj <wangjajihooon@gmail.com>
2024-12-20 15:54:10 +0900

    feat: jwt 관련 코드 java로 작성

--- src/main/kotlin/com/jova/domain/auth/dto/response/TokenResponse.java
+++ src/main/kotlin/com/jova/domain/auth/dto/response/TokenResponse.java
@@ -1,14 +1,21 @@
 package com.jova.domain.auth.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponse {
     String accessToken;
     String refreshToken;
+    String grantType;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime accessTokenExpiration;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
